bdfaba9a995030548356cd14092361ec9a38b122
Ruben Fortes-Marques <ruben.fortes-marques@iesdoctorbalmis.com>
2022-03-12 11:23:20 +0100

    Añado toolkit mvvm y creo carpeta para las clases modelo

---
+++ Proyecto_Xarxa_Desktop/modelo/Alumno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Xarxa_Desktop.modelo
+{
+    class Alumno
+    {
+        private int nia;
+
+        public int Nia
+        {
+            get { return nia; }
+            set { nia = value; }
+        }
+
+    }
+}
