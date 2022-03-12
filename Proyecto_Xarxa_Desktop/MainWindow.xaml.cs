b14e5d21b71cbba83b43906696711546b7fd3975
Ruben Fortes-Marques <ruben.fortes-marques@iesdoctorbalmis.com>
2022-03-12 11:18:36 +0100

    Primer commit rama ruben con servicio lectura csv

--- Proyecto_Xarxa_Desktop/MainWindow.xaml.cs
+++ Proyecto_Xarxa_Desktop/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Proyecto_Xarxa_Desktop.servicios;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace Proyecto_Xarxa_Desktop
         public MainWindow()
         {
             InitializeComponent();
+            ServicioCsv.LeeCsv();
         }
     }
 }
---
+++ Proyecto_Xarxa_Desktop/servicios/ServicioCsv.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Xarxa_Desktop.servicios
+{
+    class ServicioCsv
+    {
+        public static void LeeCsv()
+        {
+            StreamReader archivo = new StreamReader(Properties.Settings.Default.UbicacionCsv);
+            archivo.ReadLine(); // Leer la primera línea para descartarla porque es el encabezado
+            String linea = "";
+            Char separador = ',';
+            while ((linea = archivo.ReadLine()) != null)
+            {
+                string[] fila = linea.Split(separador);
+                Console.WriteLine(fila);
+            }
+        }
+    }
+}
