6d7e8ec93772330e19ff581b2171e1c2dcf9763c
Ruben Fortes-Marques <ruben.fortes-marques@iesdoctorbalmis.com>
2022-03-27 16:56:33 +0200

    Creados ServicioAPI (con método GetAll para Lotes funcional) y ServicioNavegacion

---
+++ Proyecto_Xarxa_Desktop/servicios/ServicioAPI.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Newtonsoft.Json;
+using Proyecto_Xarxa_Desktop.modelo;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Xarxa_Desktop.servicios
+{
+    class ServicioAPI
+    {
+        private static ObservableCollection<Lote> listaLotes = new ObservableCollection<Lote>();
+        public static ObservableCollection<Lote> GetLotes()
+        {
+            var url = $"http://localhost:8081/apixarxa/xarxa/lotes";
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.ContentType = "application/json";
+            request.Accept = "application/json";
+            try
+            {
+                using (WebResponse response = request.GetResponse())
+                {
+                    using (Stream strReader = response.GetResponseStream())
+                    {
+                        if (strReader == null) return null;
+                        using (StreamReader objReader = new StreamReader(strReader))
+                        {
+                            string responseBody = objReader.ReadToEnd();
+                            listaLotes = JsonConvert.DeserializeObject<ObservableCollection<Lote>>(responseBody);
+                            return listaLotes;
+                        }
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Proyecto_Xarxa_Desktop/servicios/ServicioNavegacion.cs
@@ -0,0 +1,19 @@
+﻿using Proyecto_Xarxa_Desktop.vistas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Proyecto_Xarxa_Desktop.servicios
+{
+    internal class ServicioNavegacion
+    {
+        public ServicioNavegacion()
+        { }
+
+        // Devuelve un nuevo UserControl de Lotes
+        internal static UserControl AbrirVistaLotes() => new LotesUserControl();
+    }
+}
