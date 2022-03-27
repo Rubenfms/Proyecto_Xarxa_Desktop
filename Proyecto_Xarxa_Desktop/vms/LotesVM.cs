21774cbbdbe9f15a508a46b1af0ca0f3d78d30ba
Ruben Fortes-Marques <ruben.fortes-marques@iesdoctorbalmis.com>
2022-03-27 16:56:01 +0200

    Creados vms de vista Lotes y MainWindow

---
+++ Proyecto_Xarxa_Desktop/vms/LotesVM.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Proyecto_Xarxa_Desktop.modelo;
+using Proyecto_Xarxa_Desktop.servicios;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Xarxa_Desktop.vms
+{
+    class LotesVM : ObservableObject
+    {
+        private ObservableCollection<Lote> listaLotes;
+
+        public ObservableCollection<Lote> ListaLotes
+        {
+            get { return listaLotes; }
+            set { SetProperty(ref listaLotes, value); }
+        }
+
+        public LotesVM()
+        {
+            listaLotes = ServicioAPI.GetLotes();
+        }
+    }
+}
---
+++ Proyecto_Xarxa_Desktop/vms/MainWindowVM.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Proyecto_Xarxa_Desktop.servicios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Proyecto_Xarxa_Desktop.vms
+{
+    class MainWindowVM : ObservableObject
+    {
+        private UserControl pestanyaActual;
+
+        public UserControl PestanyaActual
+        {
+            get { return pestanyaActual; }
+            set { SetProperty(ref pestanyaActual, value); }
+        }
+
+        public MainWindowVM()
+        {
+            // Instancia vacía para que al iniciar el programa no aparezca ningún UserControl seleccionado
+            PestanyaActual = new UserControl();
+        }
+
+        public void AbrirVistaLotes() => PestanyaActual = ServicioNavegacion.AbrirVistaLotes();
+        
+    }
+}
