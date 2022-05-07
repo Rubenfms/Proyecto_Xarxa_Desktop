2772efdde59d36374c846f78579045af8e02ad78
Joa <ja.cutillas@iesdoctorbalmis.com>
2022-05-07 09:51:25 +0200

    Instalo paquete codigo de barras

---
+++ Proyecto_Xarxa_Desktop/servicios/ServicioCodigoBarras.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Xarxa_Desktop.servicios
+{
+    class ServicioCodigoBarras
+    {
+
+
+       // BarcodeWriter.CreateBarcode("https://ironsoftware.com/csharp/barcode", BarcodeWriterEncoding.QRCode).SaveAsJpeg("QuickStart.jpg");
+
+
+       // BarcodeResult Result = BarcodeReader.QuicklyReadOneBarcode("QuickStart.jpg");
+    }
+}
