f2a4ee1864f2ef8eb59d2bb26fc4d80bbd1064a6
Ruben Fortes-Marques <ruben.fortes-marques@iesdoctorbalmis.com>
2022-05-25 17:57:18 +0200

    Arreglado bug que abria lote vacio al pulsar editar y bug en eliminar libro

--- Proyecto_Xarxa_Desktop/vms/AnyadirLibroVM.cs
+++ Proyecto_Xarxa_Desktop/vms/AnyadirLibroVM.cs
@@ -16,88 +16,20 @@ namespace Proyecto_Xarxa_Desktop.vms
     class AnyadirLibroVM : ObservableObject
     {
         /// <summary>
-        /// ISBN que identifica al libro.
+        /// Libro que vamos a crear a través de los datos introducidos en el formulario.
         /// </summary>
-        private string isbn;
+        private Libro libroSel;
 
         /// <summary>
-        /// Gets or sets el isbn.
+        /// Gets or sets the libro sel.
         /// </summary>
         /// <value>
-        /// ISBN que identifica al libro.
+        /// Libro que vamos a crear a través de los datos introducidos en el formulario.
         /// </value>
-        public string Isbn
+        public Libro LibroSel
         {
-            get { return isbn; }
-            set { SetProperty(ref isbn, value); }
-        }
-
-        /// <summary>
-        /// El titulo del libro.
-        /// </summary>
-        private string titulo;
-
-        /// <summary>
-        /// Gets or sets el titulo del libro.
-        /// </summary>
-        /// <value>
-        /// El titulo del libro.
-        /// </value>
-        public string Titulo
-        {
-            get { return titulo; }
-            set { SetProperty(ref titulo, value); }
-        }
-
-        /// <summary>
-        /// Curso en el que se usará el libro.
-        /// </summary>
-        private string curso;
-
-        /// <summary>
-        /// Gets or sets el curso del libro.
-        /// </summary>
-        /// <value>
-        /// Curso en el que se usará el libro.
-        /// </value>
-        public string Curso
-        {
-            get { return curso; }
-            set { SetProperty(ref curso, value); }
-        }
-
-        /// <summary>
-        /// La editorial del libro.
-        /// </summary>
-        private string editorial;
-
-        /// <summary>
-        /// Gets or sets la editorial.
-        /// </summary>
-        /// <value>
-        /// La editorial del libro.
-        /// </value>
-        public string Editorial
-        {
-            get { return editorial; }
-            set { SetProperty(ref editorial, value); }
-        }
-
-        /// <summary>
-        /// El departamento al que pertenece el libro
-        /// </summary>
-        private string departamento;
-
-        /// <summary>
-        /// Gets or sets el departamento del libro.
-        /// </summary>
-        /// <value>
-        /// El departamento al que pertenece el libro.
-        /// </value>
-        public string Departamento
-        {
-            get { return departamento; }
-            set { SetProperty(ref departamento, value); }
+            get { return libroSel; }
+            set { SetProperty(ref libroSel, value); }
         }
 
         /// <summary>
@@ -109,7 +41,7 @@ namespace Proyecto_Xarxa_Desktop.vms
         /// </summary>
         public AnyadirLibroVM()
         {
-
+            LibroSel = new Libro();
         }
 
         /// <summary>
@@ -119,7 +51,7 @@ namespace Proyecto_Xarxa_Desktop.vms
         {
             if(ComprobarDatosIntroducidos())
             {
-                HttpStatusCode? statusCode = servicioAPI.PostLibro(new Libro(Isbn, Titulo, Curso, Departamento, Editorial));
+                HttpStatusCode? statusCode = servicioAPI.PostLibro(libroSel);
                 ServicioDialogos.ServicioMessageBox($"Resultado del alta del libro: {statusCode}", "Resultado alta", MessageBoxButton.OK, MessageBoxImage.Information);
                 WeakReferenceMessenger.Default.Send(new DatoAñadidoOModificadoMessage(statusCode == HttpStatusCode.Created));
                 return true;
@@ -133,27 +65,29 @@ namespace Proyecto_Xarxa_Desktop.vms
         /// <returns>true si todos los datos introducidos o false si algún dato érroneo</returns>
         public bool ComprobarDatosIntroducidos()
         {
-            if (Isbn == null || Int32.TryParse(Isbn, out _) || Isbn.Length < 10)
+            bool isbnSoloNumeros = long.TryParse(LibroSel.Isbn, out _);
+
+            if (LibroSel.Isbn == null || !isbnSoloNumeros || LibroSel.Isbn.Length < 10)
             {
-                ServicioDialogos.ServicioMessageBox("El formato de ISBN introducido no es válido (Ej:978963516711). Prueba a introducir solo números y que el ISBN sea mayor de 10 digitos.", "Formato no válido", MessageBoxButton.OK, MessageBoxImage.Information);
+                ServicioDialogos.ServicioMessageBox("El formato de ISBN introducido no es válido. Prueba a introducir solo números y que el ISBN sea mayor de 10 digitos. (Ej:978963516711)", "Formato no válido", MessageBoxButton.OK, MessageBoxImage.Information);
                 return false;
             }
-            else if (Titulo == null || Titulo.Length == 0)
+            else if (LibroSel.Titulo == null || LibroSel.Titulo.Length == 0)
             {
                 ServicioDialogos.ServicioMessageBox("Introduce el título del libro.", "Formato no válido", MessageBoxButton.OK, MessageBoxImage.Information);
                 return false;
             }
-            else if (Curso == null || Curso.Length == 0)
+            else if (LibroSel.Curso == null || LibroSel.Curso.Length == 0)
             {
                 ServicioDialogos.ServicioMessageBox("Introduce el curso en el que se usará el libro.", "Formato no válido", MessageBoxButton.OK, MessageBoxImage.Information);
                 return false;
             }
-            else if (Editorial == null || Editorial.Length == 0)
+            else if (LibroSel.Editorial == null || LibroSel.Editorial.Length == 0)
             {
                 ServicioDialogos.ServicioMessageBox("Introduce la editorial del libro.", "Formato no válido", MessageBoxButton.OK, MessageBoxImage.Information);
                 return false;
             }
-            else if (Departamento == null || Departamento.Length == 0)
+            else if (LibroSel.Departamento == null || LibroSel.Departamento.Length == 0)
             {
                 ServicioDialogos.ServicioMessageBox("Introduce el departamento del libro.", "Formato no válido", MessageBoxButton.OK, MessageBoxImage.Information);
                 return false;
