3d4f30b7216be61333cf2e5c7065ec4ea70eeaaa
Ruben Fortes-Marques <ruben.fortes-marques@iesdoctorbalmis.com>
2022-05-25 19:41:47 +0200

    Documentado modelo numero

--- Proyecto_Xarxa_Desktop/modelo/Numero.cs
+++ Proyecto_Xarxa_Desktop/modelo/Numero.cs
@@ -10,24 +10,47 @@ namespace Proyecto_Xarxa_Desktop.modelo
 {
     class Numero : ObservableObject
     {
+        /// <summary>
+        /// Contenido del número
+        /// </summary>
         long contenido;
 
+        /// <summary>
+        /// Gets or sets the contenido.
+        /// </summary>
+        /// <value>
+        /// Contenido del número
+        /// </value>
         [JsonProperty("numero")]
         public long Contenido
         {
             get { return contenido; }
             set { SetProperty(ref contenido, value); }
         }
+
+        /// <summary>
+        /// Converts to string.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
         public override string ToString()
         {
             return base.ToString();
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Numero"/> class.
+        /// </summary>
+        /// <param name="contenido">The contenido.</param>
         public Numero(long contenido)
         {
             Contenido = contenido;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Numero"/> class.
+        /// </summary>
         public Numero()
         {
         }
