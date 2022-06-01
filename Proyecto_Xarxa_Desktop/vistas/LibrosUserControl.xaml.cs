4c47f56a1a8edc0f388ff7c1f10487df55bc4019
Ruben Fortes-Marques <ruben.fortes-marques@iesdoctorbalmis.com>
2022-06-01 18:29:28 +0200

    añadido filtro para los libros

--- Proyecto_Xarxa_Desktop/vistas/LibrosUserControl.xaml.cs
+++ Proyecto_Xarxa_Desktop/vistas/LibrosUserControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Proyecto_Xarxa_Desktop.vms;
+﻿using Proyecto_Xarxa_Desktop.modelo;
+using Proyecto_Xarxa_Desktop.vms;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -33,6 +34,25 @@ namespace Proyecto_Xarxa_Desktop.vistas
         {
             InitializeComponent();
             DataContext = vm;
+        }
+
+        private void Buscador_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            listview.ClearValue(ListView.ItemsSourceProperty); // Eliminamos el valor de la propiedad Items Source para poder usar Clear items
+            listview.Items.Clear(); // Eliminamos los items actuales antes de añadir los nuevos filtrados
+
+
+            // Filtramos y mostramos el resultado filtrado
+            string filtro = Buscador.Text.ToUpper();
+            foreach (Libro l in vm.ListaLibros)
+            {
+                if (l.Isbn.ToUpper().Contains(filtro) || l.Titulo.ToUpper().Contains(filtro) ||
+                    l.Departamento.ToUpper().Contains(filtro) || l.Editorial.ToUpper().Contains(filtro) ||
+                    l.Curso.ToString().ToUpper().Contains(filtro))
+                {
+                    listview.Items.Add(l);
+                }
+            }
         }
 
         private void EditarLibroButton_Click(object sender, RoutedEventArgs e)
