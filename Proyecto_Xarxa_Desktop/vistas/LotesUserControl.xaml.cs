4bf50b11532fbd2ef69dbed3864203c82e9960ef
Ruben Fortes-Marques <ruben.fortes-marques@iesdoctorbalmis.com>
2022-06-01 18:57:23 +0200

    filtro en pantalla de lotes

--- Proyecto_Xarxa_Desktop/vistas/LotesUserControl.xaml.cs
+++ Proyecto_Xarxa_Desktop/vistas/LotesUserControl.xaml.cs
@@ -37,6 +37,11 @@ namespace Proyecto_Xarxa_Desktop.vistas
             DataContext = vm;
         }
 
+        /// <summary>
+        /// Handles the TextChanged event of the Buscador control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="TextChangedEventArgs"/> instance containing the event data.</param>
         private void Buscador_TextChanged(object sender, TextChangedEventArgs e)
         {
             listview.ClearValue(ListView.ItemsSourceProperty); // Eliminamos el valor de la propiedad Items Source para poder usar Clear items
@@ -55,9 +60,73 @@ namespace Proyecto_Xarxa_Desktop.vistas
             }
         }
 
+
         private void CrearModalidadButton_Click(object sender, RoutedEventArgs e)
         {
             vm.AbrirVistaCrearModalidad();
+        }
+
+        /// <summary>
+        /// Handles the Click event of the LotesAsignadosRadioButton control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private void LotesAsignadosRadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            listview.ClearValue(ListView.ItemsSourceProperty); // Eliminamos el valor de la propiedad Items Source para poder usar Clear items
+            listview.Items.Clear(); // Eliminamos los items actuales antes de añadir los nuevos filtrados
+
+
+            // Filtramos y mostramos el resultado filtrado
+            string filtro = Buscador.Text.ToUpper();
+            foreach (Lote l in vm.ListaLotes)
+            {
+                if (l.IdLote > 0)
+                {
+                    listview.Items.Add(l);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Handles the Click event of the LotesNoAsignadosRadioButton control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private void LotesNoAsignadosRadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            listview.ClearValue(ListView.ItemsSourceProperty); // Eliminamos el valor de la propiedad Items Source para poder usar Clear items
+            listview.Items.Clear(); // Eliminamos los items actuales antes de añadir los nuevos filtrados
+
+
+            // Filtramos y mostramos el resultado filtrado
+            string filtro = Buscador.Text.ToUpper();
+            foreach (Lote l in vm.ListaLotes)
+            {
+                if (l.IdLote == 0)
+                {
+                    listview.Items.Add(l);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Handles the Click event of the SinFiltroRadioButton control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private void SinFiltroRadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            listview.ClearValue(ListView.ItemsSourceProperty); // Eliminamos el valor de la propiedad Items Source para poder usar Clear items
+            listview.Items.Clear(); // Eliminamos los items actuales antes de añadir los nuevos filtrados
+
+
+            // Filtramos y mostramos el resultado filtrado
+            string filtro = Buscador.Text.ToUpper();
+            foreach (Lote l in vm.ListaLotes)
+            {
+                listview.Items.Add(l);   
+            }
         }
     }
 }
